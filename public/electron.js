ef89053110583ec9880b8dd16bd5ec6b961b085e
huntermcnicholsmbp <hmcnich@gmail.com>
2022-11-03 23:00:32 -0400

    Initial POC UI

--- public/electron.js
+++ public/electron.js
@@ -6,8 +6,8 @@ const isDev = require('electron-is-dev');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1152,
+    height: 655,
     webPreferences: {
       nodeIntegration: true,
     },
