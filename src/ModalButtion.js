9cb6b82b45cd7fd339019ae15a6c267f14fe7601
jwlee-jake <jaewooklee.jake@gmail.com>
2022-11-06 10:21:00 -0500

    Added interfaces. Now need to add boiler function.

--- src/ModalButtion.js
+++ src/ModalButtion.js
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+import Stack from '@mui/material/Stack';
 
 const style = {
     position: 'absolute',
@@ -20,11 +23,42 @@ class ModalButtion extends Component {
     constructor(props) {
         super(props); 
         this.state = {
-            open: false
+            open: false,
+            score: 0,
+            feedback: "",
+            title: ""
         }
         this.handleOpen = this.handleOpen.bind(this);
         this.handleClose = this.handleClose.bind(this);
     }
+
+    handleScoreChange(event, newScore) {
+        if (newScore > 100){
+            newScore = 100;
+        } 
+        if (newScore < 0) {
+            newScore = 0;  
+        }
+        let newState = this.state;
+        newState.score = newScore;
+        console.log("New score: ", newScore)
+        this.setState(newState)
+    }
+
+    handleFeedbackChange(event, newFeedback) {
+        console.log("New feedback: ", newFeedback)
+        let newState = this.state;
+        newState.feedback = newFeedback;
+        this.setState(newState)
+    }
+
+    handleTitleChange(event, newTitle) {
+        console.log("New title: ", newTitle)
+        let newState = this.state;
+        newState.title = newTitle;
+        this.setState(newState)
+    }
+
 
     handleOpen() {
         this.setState({open: true})
@@ -44,13 +78,50 @@ class ModalButtion extends Component {
                     aria-describedby="modal-modal-description"
                 >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                </Box>
+                <Stack spacing={2}>
+                    <TextField 
+                        id="standard-basic" 
+                        label="Title" variant="standard" 
+                        value={this.state.title}
+                        onChange={(event) => this.handleTitleChange(event, event.target.value)}
+                    />
+                    <Box sx={{display:"flex", justifyContent:"space-around"}}>
+                        <Box>
+                            <Typography variant='h7' display="inline"> Points Deducted</Typography>
+                            <Input
+                                    px={3}
+                                    sx={{
+                                        '& input' : {
+                                            textAlign:"right !important",
+                                            }
+                                        }}
+                                        
+                                    value={this.state.score}
+                                    size="small"
+                                    onChange={(event) => this.handleScoreChange(event, event.target.value)}
+                                    inputProps={{
+                                        step: 1,
+                                        min: 0,
+                                        max: 100,
+                                        type:'number',
+                                        sx:{textAlign:"right"}
+                                    }}
+                                />
+                            </Box>
+                        </Box>
+
+                        <TextField
+                            id="standard-multiline-static"
+                            label="Feedback"
+                            multiline
+                            maxRows={4}
+                            value={this.state.feedback}
+                            onChange={(event) => this.handleFeedbackChange(event, event.target.value)}
+                            variant="standard"
+                        />
+                        <Button variant="contained">Submit</Button>
+                        </Stack>
+                    </Box>
                 </Modal>
         </div>
         )
