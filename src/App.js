3c68d675054cba5f922d571704e8f95b08319e78
Dzung Pham <24899880+dzungvpham@users.noreply.github.com>
2022-11-07 17:06:15 -0500

    Reorganized some files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import GitHubButton from './components/githubButton';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import FeedbackNav from './components/FeedbackNav'
-import FeedbackInfoPanel from './components/FeedbackInfoPanel';
-import StudentNavigation from './components/StudentNavigation';
+import AppContainer from './components/AppContainer';
 
 function App() {
   return (
     <div className="App">
-      <Box>
-      <Grid container sx={{height:'100vh'}}>
-          <Grid xs={4}>
-            <FeedbackNav/>
-          </Grid>
-          <Grid xs={8} sx={{background:"#eeeeee", height:"100vh"}}>
-            <Box>
-              <StudentNavigation/>
-              <FeedbackInfoPanel/>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
+      <AppContainer/>
     </div>
   );
 }
